f4a7ebf6f5bfa7a5eb2328a3e8188f80a3d5300e
NijinAddons <nijin@addon-s.com>
2026-08-05 10:38:07 +0400

    sales order creation

--- buy_in_minutes/payment.py
+++ buy_in_minutes/payment.py
@@ -8,7 +8,7 @@ from urllib.request import Request, urlopen
 
 import frappe
 from frappe import _
-from frappe.utils import flt, get_url
+from frappe.utils import flt, get_url, nowdate
 
 
 STRIPE_API_BASE_URL = "https://api.stripe.com/v1"
@@ -17,6 +17,8 @@ HANDLING_FEE = 2
 DELIVERY_FEE = 6
 FREE_DELIVERY_MINIMUM = 60
 SELLING_PRICE_LIST = "Selling Price"
+DEFAULT_CUSTOMER_GROUP = "All Customer Groups"
+DEFAULT_TERRITORY = "All Territories"
 
 
 def _error(message, status_code=400):
@@ -118,6 +120,75 @@ def _get_item_selling_price(item_code, fallback_rate=0):
 	return flt(price_record.price_list_rate if price_record else fallback_rate)
 
 
+def _get_default_company():
+	company = frappe.defaults.get_user_default("Company") or frappe.defaults.get_global_default("company")
+	if company:
+		return company
+
+	company = frappe.db.get_value("Company", {}, "name")
+	if not company:
+		frappe.throw(_("Please configure a default Company before checkout."))
+
+	return company
+
+
+def _get_default_customer_group():
+	customer_group = frappe.defaults.get_global_default("customer_group")
+	if customer_group:
+		return customer_group
+
+	if frappe.db.exists("Customer Group", DEFAULT_CUSTOMER_GROUP):
+		return DEFAULT_CUSTOMER_GROUP
+
+	customer_group = frappe.db.get_value("Customer Group", {"is_group": 0}, "name")
+	if not customer_group:
+		frappe.throw(_("Please configure a Customer Group before checkout."))
+
+	return customer_group
+
+
+def _get_default_territory():
+	territory = frappe.defaults.get_global_default("territory")
+	if territory:
+		return territory
+
+	if frappe.db.exists("Territory", DEFAULT_TERRITORY):
+		return DEFAULT_TERRITORY
+
+	territory = frappe.db.get_value("Territory", {"is_group": 0}, "name")
+	if not territory:
+		frappe.throw(_("Please configure a Territory before checkout."))
+
+	return territory
+
+
+def _get_or_create_customer_for_user(user_name):
+	user = frappe.get_doc("User", user_name)
+	customer_name = None
+	if user.email:
+		customer_name = frappe.db.get_value("Customer", {"email_id": user.email}, "name")
+	if not customer_name and user.mobile_no:
+		customer_name = frappe.db.get_value("Customer", {"mobile_no": user.mobile_no}, "name")
+
+	if customer_name:
+		return customer_name
+
+	customer_doc = frappe.get_doc(
+		{
+			"doctype": "Customer",
+			"customer_name": user.full_name or user.email or user.mobile_no or user.name,
+			"customer_type": "Individual",
+			"customer_group": _get_default_customer_group(),
+			"territory": _get_default_territory(),
+			"email_id": user.email,
+			"mobile_no": user.mobile_no,
+		}
+	)
+	customer_doc.insert(ignore_permissions=True)
+
+	return customer_doc.name
+
+
 def _get_checkout_items(cart_items):
 	checkout_items = []
 	for cart_item in _normalize_cart_items(cart_items):
@@ -170,8 +241,67 @@ def _get_checkout_items(cart_items):
 	return checkout_items
 
 
-def _build_checkout_params(checkout_items):
-	success_url = get_url("/buy-in-minutes#/payment/success?session_id={CHECKOUT_SESSION_ID}")
+def _build_sales_order_item_rows(checkout_items):
+	order_items = []
+	for item in checkout_items:
+		if not frappe.db.exists("Item", item["item_code"]):
+			continue
+
+		order_items.append(
+			{
+				"item_code": item["item_code"],
+				"item_name": item["item_name"],
+				"qty": item["quantity"],
+				"rate": item["rate"],
+				"delivery_date": nowdate(),
+			}
+		)
+
+	return order_items
+
+
+def _upsert_sales_order(checkout_items, sales_order_name=None, submit=False):
+	customer = _get_or_create_customer_for_user(frappe.session.user)
+	company = _get_default_company()
+	delivery_date = nowdate()
+	order_items = _build_sales_order_item_rows(checkout_items)
+
+	if not order_items:
+		frappe.throw(_("Cart does not contain orderable items."))
+
+	if sales_order_name and frappe.db.exists("Sales Order", sales_order_name):
+		sales_order = frappe.get_doc("Sales Order", sales_order_name)
+		if sales_order.docstatus != 0:
+			frappe.throw(_("The linked Sales Order is no longer editable."))
+		sales_order.set("items", [])
+	else:
+		sales_order = frappe.get_doc({"doctype": "Sales Order"})
+
+	sales_order.update(
+		{
+			"customer": customer,
+			"company": company,
+			"transaction_date": nowdate(),
+			"delivery_date": delivery_date,
+			"order_type": "Sales",
+		}
+	)
+	for item in order_items:
+		sales_order.append("items", item)
+
+	if sales_order.is_new():
+		sales_order.insert(ignore_permissions=True)
+	else:
+		sales_order.save(ignore_permissions=True)
+
+	if submit:
+		sales_order.submit()
+
+	return sales_order
+
+
+def _build_checkout_params(checkout_items, sales_order_name=None):
+	success_url = get_url("/buy-in-minutes#/payment/success?method=stripe&session_id={CHECKOUT_SESSION_ID}")
 	cancel_url = get_url("/buy-in-minutes#/payment/cancel")
 	params = {
 		"mode": "payment",
@@ -179,7 +309,10 @@ def _build_checkout_params(checkout_items):
 		"cancel_url": cancel_url,
 		"client_reference_id": frappe.session.user,
 		"metadata[user]": frappe.session.user,
+		"metadata[payment_method]": "stripe",
 	}
+	if sales_order_name:
+		params["metadata[sales_order]"] = sales_order_name
 
 	for index, item in enumerate(checkout_items):
 		unit_amount = int(round(item["rate"] * 100))
@@ -193,17 +326,52 @@ def _build_checkout_params(checkout_items):
 
 
 @frappe.whitelist(methods=["POST"])
-def create_checkout_session(cart_items=None):
+def create_checkout_session(cart_items=None, sales_order_name=None):
 	if frappe.session.user == "Guest":
 		frappe.throw(_("Please sign in before checkout."), frappe.AuthenticationError)
 
 	checkout_items = _get_checkout_items(cart_items)
-	session = _stripe_request("/checkout/sessions", _build_checkout_params(checkout_items))
+	sales_order = _upsert_sales_order(checkout_items, sales_order_name=sales_order_name)
+	session = _stripe_request(
+		"/checkout/sessions",
+		_build_checkout_params(checkout_items, sales_order.name),
+	)
 
 	return {
 		"success": True,
 		"checkout_url": session.get("url"),
 		"session_id": session.get("id"),
+		"sales_order": sales_order.name,
+	}
+
+
+@frappe.whitelist(methods=["POST"])
+def create_cash_on_delivery_order(cart_items=None, sales_order_name=None):
+	if frappe.session.user == "Guest":
+		frappe.throw(_("Please sign in before checkout."), frappe.AuthenticationError)
+
+	checkout_items = _get_checkout_items(cart_items)
+	sales_order = _upsert_sales_order(checkout_items, sales_order_name=sales_order_name, submit=True)
+
+	return {
+		"success": True,
+		"sales_order": sales_order.name,
+		"payment_method": "cod",
+	}
+
+
+@frappe.whitelist(methods=["POST"])
+def sync_draft_sales_order(cart_items=None, sales_order_name=None):
+	if frappe.session.user == "Guest":
+		frappe.throw(_("Please sign in before syncing your cart."), frappe.AuthenticationError)
+
+	checkout_items = _get_checkout_items(cart_items)
+	sales_order = _upsert_sales_order(checkout_items, sales_order_name=sales_order_name)
+
+	return {
+		"success": True,
+		"sales_order": sales_order.name,
+		"status": sales_order.status,
 	}
 
 
@@ -253,6 +421,8 @@ def stripe_webhook():
 					"session_id": session.get("id"),
 					"payment_status": session.get("payment_status"),
 					"user": session.get("metadata", {}).get("user"),
+					"payment_method": session.get("metadata", {}).get("payment_method"),
+					"sales_order": session.get("metadata", {}).get("sales_order"),
 				},
 				indent=2,
 			),
