a72848c8f3ee283578cff95765da21b821528751
NijinAddons <nijin@addon-s.com>
2026-08-12 11:25:36 +0400

    autoscroll and PO fix

--- buy_in_minutes/__init__.py
+++ buy_in_minutes/__init__.py
@@ -1,1 +1,10 @@
 __version__ = "0.0.1"
+
+import frappe
+
+
+@frappe.whitelist(methods=["POST"])
+def create_cash_on_delivery_order(cart_items=None, sales_order_name=None):
+	from buy_in_minutes.payment import create_cash_on_delivery_order as create_order
+
+	return create_order(cart_items=cart_items, sales_order_name=sales_order_name)
