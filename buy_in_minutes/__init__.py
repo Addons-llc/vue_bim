6d77d3731ac07edbe6afaef0fbdcf7e3b807423a
NijinAddons <nijin@addon-s.com>
2026-08-13 15:44:41 +0400

    fix

--- buy_in_minutes/__init__.py
+++ buy_in_minutes/__init__.py
@@ -7,4 +7,7 @@ import frappe
 def create_cash_on_delivery_order(cart_items=None, sales_order_name=None):
 	from buy_in_minutes.payment import create_cash_on_delivery_order as create_order
 
+
+
+
 	return create_order(cart_items=cart_items, sales_order_name=sales_order_name)
