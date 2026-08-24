47e57b4eed06f2813540bd3a931bda66e2371602
NijinAddons <nijin@addon-s.com>
2026-08-24 12:21:59 +0400

    variants card fix

--- frontend/src/pages/BrandDetailsPage.vue
+++ frontend/src/pages/BrandDetailsPage.vue
@@ -229,8 +229,6 @@ watch(brandBannerImages, (images) => {
         </div>
       </section>
 
-
-
       <section class="supplier-products-panel">
         <h2>Products by {{ brandDisplayName }}</h2>
         <p v-if="isLoadingBrandProducts" class="dashboard-message">
